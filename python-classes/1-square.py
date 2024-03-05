c89b7137d006c64ce46f41726fda62e121298563
Fidele012 <ndihokubwafidele@gmail.com>
2024-03-05 15:06:54 +0300

    recuperation

---
+++ python-classes/1-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+1-square.py [unix] (14:59 05/03/2024)                                                                                                                                                                      0,1 All
+-- INSERT --
+
